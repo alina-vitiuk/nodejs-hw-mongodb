79d0a8e86c83ce3cca1e1858b0658f9f96eaea59
alina-vitiuk <161250475+alina-vitiuk@users.noreply.github.com>
2025-03-11 22:54:54 +0200

    Update initMongoConnection.js

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -10,10 +10,7 @@ export const initMongoConnection = async () => {
   const DB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
 
   try {
-    await mongoose.connect(DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(DB_URI);
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
